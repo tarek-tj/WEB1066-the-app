2178d6d2488170c1f2dcba90af926c6396619011
tarek-tj <53327878+tarek-tj@users.noreply.github.com>
2020-07-20 15:23:56 +0300

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,5 +3,5 @@ set -e
 
 echo "Starting deploy"
 echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_LOGIN}" --password-stdin
-docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
-docker push $DOCKER_LOGIN/monolithic-shop:latest
+docker tag tarek47/web1066-the-app:latest $DOCKER_LOGIN/web1066-the-app:latest
+docker push $DOCKER_LOGIN/web1066-the-app:latest
